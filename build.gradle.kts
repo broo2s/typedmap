691b3ec4d17111be3ab77bab569c913f757f5e27
Ryszard Wiśniewski <brut.alll@gmail.com>
2021-05-20 15:09:19 +0200

    [gradle] Generate the project version from git tags and other data.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,22 @@
+import com.palantir.gradle.gitversion.VersionDetails
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.5.0"
     id("org.jetbrains.dokka") version "1.4.32"
+    id("com.palantir.git-version") version "0.12.3" apply false
 }
 
+try {
+    apply(plugin = "com.palantir.git-version")
+} catch (e: Exception) {
+    project.logger.warn(e.message, e)
+}
+
+val projectVersion = createProjectVersion()
+
 group = "me.broot.typedmap"
-version = "1.0.0-SNAPSHOT"
+version = projectVersion.name
 
 subprojects {
     apply(plugin = "org.jetbrains.kotlin.jvm")
@@ -33,3 +43,42 @@ allprojects {
         mavenCentral()
     }
 }
+
+fun Project.createProjectVersion() : ProjectVersion {
+    if (!extra.has("versionDetails")) {
+        return ProjectVersion("unknown", false, null)
+    }
+
+    val versionDetails: groovy.lang.Closure<VersionDetails> by extra
+    val details = versionDetails(mapOf("prefix" to "release/"))
+
+    val name = buildString {
+        append(details.lastTag)
+
+        if (details.commitDistance != 0) {
+            details.branchName?.let {
+                append('-')
+                append(it.replace('/', '-'))
+            }
+
+            append('-')
+            append(details.gitHash)
+        }
+
+        if (!details.isCleanTag) {
+            append("-dirty")
+        }
+    }
+
+    return ProjectVersion(
+        name,
+        details.commitDistance == 0 && details.isCleanTag,
+        details
+    )
+}
+
+data class ProjectVersion(
+    val name: String,
+    val isRelease: Boolean,
+    val details: VersionDetails?
+)
